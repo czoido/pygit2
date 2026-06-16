66d8f0ee8517116a25fa219068e2bc8efe8ce242
Carlos Zoido <mrgalleta@gmail.com>
2026-06-16 11:44:09 +0200

    wip

--- conanfile.py
+++ conanfile.py
@@ -1,17 +1,26 @@
+import sys
+import sysconfig
+
 from conan import ConanFile
-from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 
 
 class Pygit2Conan(ConanFile):
     name = "pygit2"
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeToolchain", "CMakeDeps"
 
     def layout(self):
         cmake_layout(self)
 
     def requirements(self):
         self.requires("libgit2/1.9.1")
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.cache_variables["Python3_EXECUTABLE"] = sys.executable
+        tc.cache_variables["Python3_INCLUDE_DIR"] = sysconfig.get_path("include")
+        tc.generate()
+        CMakeDeps(self).generate()
 
     def build(self):
         cmake = CMake(self)
